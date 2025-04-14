bd5163a5c64dae751b5b92da317c0567ec5c96fe
Napehh11 <abdoelhanavie@gmail.com>
2025-04-14 11:32:31 +0700

    Add upload_to_google_drive.sh 1744605151827

---
+++ upload_to_google_drive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to upload project_backup.zip to Google Drive
+
+# Replace with your Google Drive API upload command
+# Example: gdrive upload project_backup.zip
+echo "Upload command for Google Drive goes here."
